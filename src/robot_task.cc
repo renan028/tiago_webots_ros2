6e3354e8d59aa9d019760eb29a703294d7bfabd4
Renan Freitas <renan028@gmail.com>
2021-02-16 19:09:43 -0300

    fix qos in publishers and subscribers

--- src/robot_task.cc
+++ src/robot_task.cc
@@ -9,39 +9,42 @@ RobotTask::RobotTask() :
 
 RobotTask::~RobotTask() {}
 
-void RobotTask::updateLaserScan(const sensor_msgs::msg::LaserScan::SharedPtr 
-    scan) {
-  RCLCPP_INFO(this->get_logger(), "I heard");
-  sensor_msgs::msg::LaserScan msg = *scan;
-  msg.header.frame_id = "laser_frame";
-  std::vector<float> ranges = msg.ranges;
-  std::reverse(ranges.begin(), ranges.end());
-  msg.ranges = ranges;
-  laser_pub_->publish(msg);
 }
 
 void RobotTask::enableCamera() {
   camera_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-    "/camera_2D/image_raw", 10, 
+    "/camera_2D/image_raw", rclcpp::SensorDataQoS(), 
     std::bind(&RobotTask::updateImage, this, std::placeholders::_1));
 }
 
 void RobotTask::enableLidar() {
   lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-    "/Hokuyo_URG_04LX_UG01", 10, 
+    "/Hokuyo_URG_04LX_UG01", rclcpp::SensorDataQoS(),
     std::bind(&RobotTask::updateLaserScan, this, std::placeholders::_1));
-  //laser_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>("/scan", 10);
+  laser_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>("/scan", 
+    rclcpp::SensorDataQoS());
 }
 
 void RobotTask::enableWheel() {
   wheels_sub_ = this->create_subscription<sensor_msgs::msg::JointState>(
-    "/joint_states", 10, 
+    "/joint_states", rclcpp::SensorDataQoS(), 
     std::bind(&RobotTask::updateJoints, this, std::placeholders::_1));
 }
 
 void RobotTask::updateJoints(const sensor_msgs::msg::JointState::SharedPtr 
     joints) {
   wheels_ = *joints;
+}
+
+void RobotTask::updateLaserScan(const sensor_msgs::msg::LaserScan::SharedPtr 
+    scan) {
+  sensor_msgs::msg::LaserScan msg = *scan;
+  msg.header.stamp = this->now();
+  msg.header.frame_id = "laser_frame";
+  std::vector<float> ranges = msg.ranges;
+  std::reverse(ranges.begin(), ranges.end());
+  msg.ranges = ranges;
+  laser_pub_->publish(msg);
 }
 
 void RobotTask::updateImage(const sensor_msgs::msg::Image::SharedPtr image) {
